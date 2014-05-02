977cdc9695874b663bcdb6a4b188efda1266e03a
Jonathan Osmond <jonathancosmond@gmail.com>
2014-05-02 09:36:46 -0400

    Build further on client-side validation.

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,18 @@
+// Validation Logic
+var humanize = function (inputVal) {
+  "use strict";
+  switch(inputVal) {
+    case 'confirm-email':
+      return ' email confirmation';
+    case 'confirm-password':
+      return ' password confirmation';
+    case 'postal-code':
+      return ' postal code';
+    default:
+      return inputVal;
+  }
+};
+
 var validateEmail = function (input) {
   'use strict';
 //  var re = /^[^\s@]+@[^\s@]+$/;
@@ -5,23 +20,49 @@ var validateEmail = function (input) {
   return input.checkValidity();
 };
 
-var validateEmailConfirmation = function(inputVal) {
+var validateConfirmation = function(inputVal, confirmAgainst) {
   'use strict';
-  return inputVal === document.getElementById('email').value;
+
+  return inputVal === document.getElementById(confirmAgainst).value;
 };
 
 var validatePassword = function(inputVal) {
   "use strict";
-  
+  var validPasswordRe = /(?=.*\d)(?=.*[a-zA-Z])(?=.*[!#\$%&\?]).{8,}/;
+  return validPasswordRe.test(inputVal);
+
 };
+
+var validateTermsOfUse = function(input) {
+  "use strict";
+  return input.checked;
+};
+
+
+//Style-applying functions
+var applyStatusStyles = function(input, isValid, message) {
+  var errorMessageElement;
+  if (isValid) {
+      input.parentNode.className += " valid-field";
+    } else {
+      errorMessageElement = document.querySelector("." + input.id + " .error-message");
+      errorMessageElement.textContent = message;
+      errorMessageElement.className += " visible";
+      input.parentNode.className += " invalid-field";
+    }
+};
+
 var inputSwtichboard = function (input) {
   'use strict';
 
   switch (input.id) {
     case 'email':
-      return validateEmail(input);
+      applyStatusStyles(input, validateEmail(input), "Please use a valid Email Address");
+      break;
     case 'confirm-email':
-      return validateEmailConfirmation(input.value);
+      applyStatusStyles(input,
+        validateConfirmation(input.value, "email"), "This does not match the email entered above.");
+      break;
     case 'country':
       if (input.value === 'United States') {
         console.log("change field to 'zip'");
@@ -30,15 +71,19 @@ var inputSwtichboard = function (input) {
       } else {
         console.log("redirect to appropriate page");
       }
+      break;
     case 'postal-code':
       return "postal code valid?";
     case 'password':
-      console.log(input.checkValidity());
-      return "password valid?";
-    case 'password-confirm':
-      return "password confirmation valid?";
+      applyStatusStyles(input, validatePassword(input.value),
+        "Password must be 8 or more characters, contain an uppercase letter, a lowercase letter and a symbol.");
+      break;
+    case 'confirm-password':
+      applyStatusStyles(input,
+        validateConfirmation(input.value, "password"), "This does not match the password entered above");
+      break;
     case 'terms-of-use':
-      return "terms of use valid?";
+      return validateTermsOfUse(input);
   }
 };
 
@@ -46,24 +91,26 @@ var inputSwtichboard = function (input) {
 var validateInput = function (input) {
   'use strict';
   if (input.value) {
-    return inputSwtichboard(input);
+    inputSwtichboard(input);
+  } else {
+    applyStatusStyles(input, false, "Please enter your " + humanize(input.id) + ".");
   }
-
-  return "empty input";
 };
 
 var init = function () {
   'use strict';
-  var signupInputs = document.querySelectorAll("#signup input, #signup select");
+  var signupInputs = document.querySelectorAll(".signup input, .signup select");
   var signupLength = signupInputs.length;
 
   for (var i = 0; i < signupLength; i++) {
     signupInputs[i].addEventListener('focus', function () {
-
+      this.parentNode.className = this.parentNode.className.replace(/(in)?valid-field/, "").trim();
+      var errorMessageElement = document.querySelector("." + this.id + " .error-message");
+      errorMessageElement.className = errorMessageElement.className.replace("visible", '').trim();
     }, true);
 
     signupInputs[i].addEventListener('blur', function () {
-      console.log(validateInput(this));
+      validateInput(this);
     }, true);
   }
 };
